933d712e7ad376bd5c533e349a8a8dc5fc59d9e2
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-08-16 23:52:05 +0900

    💄 add title bar

--- wordlist/frontend/src/App.tsx
+++ wordlist/frontend/src/App.tsx
@@ -17,33 +17,40 @@ function App() {
 
     return (
         <div className={styles.main}>
-            <div className={styles.csvArea}>
-                <TextField
-                    label='Multiline'
-                    multiline
-                    fullWidth
-                    value={csvText}
-                    onChange={
-                        (e) => setCsvText(e.target.value)
-                    }
-                />
+            <div className={styles.titleArea}>
+                <Button variant="contained">open file</Button>
+                <Button variant="contained">prev history</Button>
+                <Button variant="contained">next history</Button>
             </div>
-            <div className={styles.htmlArea}>
-                <TextField
-                    label='Multiline'
-                    multiline
-                    value={htmlText}
-                    fullWidth
-                    onChange={
-                        (e) => setHtmlText(e.target.value)
-                    }
-                />
-            </div>
-            <div className={styles.csvToHtmlButtonArea}>
-                <Button onClick={generateHtml}>→</Button>
-            </div>
-            <div className={styles.htmlToCsvButtonArea}>
-                <Button onClick={generateCsv}>←</Button>
+            <div className={styles.contentArea}>
+                <div className={styles.csvArea}>
+                    <TextField
+                        label='Multiline'
+                        multiline
+                        fullWidth
+                        value={csvText}
+                        onChange={
+                            (e) => setCsvText(e.target.value)
+                        }
+                    />
+                </div>
+                <div className={styles.htmlArea}>
+                    <TextField
+                        label='Multiline'
+                        multiline
+                        value={htmlText}
+                        fullWidth
+                        onChange={
+                            (e) => setHtmlText(e.target.value)
+                        }
+                    />
+                </div>
+                <div className={styles.csvToHtmlButtonArea}>
+                    <Button onClick={generateHtml} variant="contained">→</Button>
+                </div>
+                <div className={styles.htmlToCsvButtonArea}>
+                    <Button onClick={generateCsv} variant="contained">←</Button>
+                </div>
             </div>
         </div>
     )
