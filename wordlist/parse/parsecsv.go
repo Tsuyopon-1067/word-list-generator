12b1c638125c4ec40187090092b3b1170d7ff73e
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-08-18 01:18:51 +0900

    ✨  complement word meaning

--- wordlist/parse/parsecsv.go
+++ wordlist/parse/parsecsv.go
@@ -4,24 +4,51 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+	"wordlist/scraping"
 )
 
 func ParseCsv(csv string) ([]Word, error) {
-	fmt.Printf("csv: %v\n", csv)
 	lines := strings.Split(csv, "\n")
 	res := make([]Word, len(lines))
 	for i, line := range lines {
-		tmp := strings.Split(line, ",")
-		if len(tmp) < 3 {
-			msg := fmt.Sprintf("too few columns: %s", tmp)
-			err := errors.New(msg)
+		word, err := createWord(line)
+		if err != nil {
 			return nil, err
 		}
-		res[i] = Word{
-			Position: strings.TrimSpace(tmp[0]),
-			Name:     strings.TrimSpace(tmp[1]),
-			Meaning:  strings.TrimSpace(tmp[2]),
-		}
+		res[i] = word
 	}
 	return res, nil
+}
+
+func createWord(line string) (Word, error) {
+	splited := strings.Split(line, ",")
+	if len(splited) < 2 {
+		msg := fmt.Sprintf("too few columns: %s", splited)
+		err := errors.New(msg)
+		return Word{}, err
+	}
+
+	position := strings.TrimSpace(splited[0])
+	name := strings.TrimSpace(splited[1])
+	meaning := ""
+	if len(splited) >= 3 {
+		meaning = strings.TrimSpace(splited[2])
+	}
+
+	if strings.TrimSpace(name) == "" {
+		msg := fmt.Sprintf("empty word %s", splited)
+		err := errors.New(msg)
+		return Word{}, err
+	}
+
+	if len(splited) == 2 || strings.TrimSpace(meaning) == "" {
+		meaning = scraping.FetchWordMeaning(name)
+		meaning = strings.TrimSpace(meaning)
+	}
+
+	return Word{
+		Position: position,
+		Name:     name,
+		Meaning:  meaning,
+	}, nil
 }
