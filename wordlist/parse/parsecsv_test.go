a88f6b60ba6e822332451a4d60cf0547fb4e56ce
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-08-18 01:26:00 +0900

    ✅ add word meaning complement test

--- wordlist/parse/parsecsv_test.go
+++ wordlist/parse/parsecsv_test.go
@@ -45,3 +45,33 @@ func TestParseCsvFailed(t *testing.T) {
 		t.Errorf("actual: %v", actual)
 	}
 }
+
+func TestParseCsvComplementOk(t *testing.T) {
+	data := `p10 10%, available
+	p20 30%, currently
+	p30 50%, annual`
+
+	actual, err := ParseCsv(data)
+	if err != nil {
+		t.Errorf("error occured: %s", err)
+		return
+	}
+	expected := []Word{
+		{Position: "p10 10%", Name: "available", Meaning: "(すぐに)利用できる、入手できる、得られる、(…に)(すぐ)利用できて、入手できて、手に入る、(…に)手に入って、手があいていて、(…に)(ひまで)会ってもらえて"},
+		{Position: "p20 30%", Name: "currently", Meaning: "現在は、今のところ、目下、一般に、広く"},
+		{Position: "p30 50%", Name: "annual", Meaning: "一年の、年々の、例年の、年一回の、年刊の、一年生の"},
+	}
+
+	isOk := true
+	for i := range actual {
+		if actual[i] != expected[i] {
+			isOk = false
+			fmt.Printf("i=%d, actual: %v\nexpected: %v\n", i, actual[i], expected[i])
+			break
+		}
+	}
+
+	if !isOk {
+		t.Errorf("actual: %v\nexpected: %v", actual, expected)
+	}
+}
