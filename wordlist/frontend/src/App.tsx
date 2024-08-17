1fdc617f81339618f183956630304c9b0853c9c1
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-08-18 03:32:00 +0900

    💄 delete front file open compoment

--- wordlist/frontend/src/App.tsx
+++ wordlist/frontend/src/App.tsx
@@ -23,69 +23,49 @@ function App() {
     Open(ext).then((res) => setState(res));
   }
 
-  // ファイル読み込みボタン
-  const FolderSelector = () => {
-    const handleFolderSelect = (event: any) => {
-      const file = event.target.files[0];
-      const reader = new FileReader();
-      reader.onload = () => {
-        if (reader.result) {
-          const filename = file.name;
-          const ext = filename.split('.').pop();
-          const content = reader.result?.toString()
-          setFileName(filename);
-          if (ext == 'csv') {
-            setCsvText(content)
-          } else if (ext == 'html') {
-            setHtmlText(content)
-          } else {
-            // TODO: トーストを出す
-          }
-        }
-      }
-      reader.readAsText(file);
-    };
-
-    return (
-      <span>
-        <input
-          type="file"
-          style={{ display: "none" }}
-          onChange={handleFolderSelect}
-          id="folder-input"
-          accept='.csv, .html'
-        />
-        <label htmlFor="folder-input">
-          <Button variant="contained" component="span">
-            フォルダを選択
-          </Button>
-        </label>
-      </span>
-    );
-  };
-
   return (
     <div className={styles.main}>
       <div className={styles.titleArea}>
-        <FolderSelector />
-        <span style={{ color: "black" }}> filename: {fileName}</span>
+        <Button
+          onClick={() => {
+            saveFile(csvText, 'csv')
+          }}
+          variant="contained"
+        >
+          csv保存
+        </Button>
+        <Button
+          onClick={() => {
+            saveFile(csvText, 'html')
+          }}
+          variant="contained"
+        >
+          html保存
+        </Button>
 
-        <Button onClick={() => {
-          saveFile(csvText, 'csv')
-        }}>csv保存</Button>
-        <Button onClick={() => {
-          saveFile(csvText, 'html')
-        }}>html保存</Button>
+        <Button
+          onClick={() => {
+            openFile('csv', setCsvText)
+          }}
+          variant="contained"
+        >
+          csv開く
+        </Button>
+        <Button
+          onClick={() => {
+            openFile('html', setHtmlText)
+          }}
+          variant="contained"
+        >
+          html開く
+        </Button>
 
-        <Button onClick={() => {
-          openFile('csv', setCsvText)
-        }}>csv開く</Button>
-        <Button onClick={() => {
-          openFile('html', setHtmlText)
-        }}>html開く</Button>
-
-        <Button>csvコピー</Button>
-        <Button>htmlコピー</Button>
+        <Button variant="contained">
+          csvコピー
+        </Button>
+        <Button variant="contained">
+          htmlコピー
+        </Button>
       </div>
       <div className={styles.contentArea}>
         <div className={styles.csvArea}>
